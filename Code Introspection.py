0be36e881223fb7483ddb030682b2b3d360dfc04
Huijun Wu <huijun.wu.2010@gmail.com>
2013-09-09 17:31:36 -0700

    add 1 file

---
+++ Code Introspection.py
@@ -0,0 +1,11 @@
+##define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00 
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+
+print dir(Vehicle)
